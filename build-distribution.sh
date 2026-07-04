0ea1577f27fe093ed0dd2a3b9ccbc1ff816c3444
thechelsuk <GitHub@thechels.uk>
2026-07-04 19:39:04 +0100

    Fix: correct the includes in the distro zip
    Signed-off-by: thechelsuk <GitHub@thechels.uk>

--- build-distribution.sh
+++ build-distribution.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # build-distribution.sh
 # Create a zip file containing extension source and assets for distribution
-zip -r distribution.zip background.ext.js background.js content.js manifest.json settings.html settings.js assets/
+zip -r distribution.zip background.ext.js background.js content.js manifest.json popup.html popup.js assets/
